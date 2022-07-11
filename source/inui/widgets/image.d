bf70b19eca69357b2e09cd8da8d21eebe68b84e1
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-07-11 22:51:07 +0200

    tint and border color

--- source/inui/widgets/image.d
+++ source/inui/widgets/image.d
@@ -1,13 +1,21 @@
+/*
+    Copyright © 2022, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
 module inui.widgets.image;
 import bindbc.imgui;
 import bindbc.opengl;
 import inmath.linalg;
 
-void uiImImage(GLuint id, vec2 size, rect textureCutout = rect(0, 0, 1, 1), vec4 tintcolor = vec4.init, vec4 bordercolor = vec4.init) {
+void uiImImage(GLuint id, vec2 size, rect textureCutout = rect(0, 0, 1, 1), vec4 tintcolor = vec4(1), vec4 bordercolor = vec4(0)) {
     igImage(
         cast(ImTextureID)id, 
         ImVec2(size.x, size.y),
         ImVec2(textureCutout.left, textureCutout.top),
         ImVec2(textureCutout.right, textureCutout.bottom),
+        ImVec4(tintcolor.x, tintcolor.y, tintcolor.z, tintcolor.w),
+        ImVec4(bordercolor.x, bordercolor.y, bordercolor.z, bordercolor.w)
     );
 }
