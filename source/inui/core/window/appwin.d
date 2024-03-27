920251c0131efaeb29f7934477a0e167e915b28b
Grillo del Mal <grillo@delmal.cl>
2024-03-27 02:04:20 -0300

    Encapsulate linux specific code.

--- source/inui/core/window/appwin.d
+++ source/inui/core/window/appwin.d
@@ -380,11 +380,13 @@ public:
     }
 
     string getWindowHandle() {
-        SDL_SysWMinfo info;
-        auto res = SDL_GetWindowWMInfo(window, &info);
-        if (info.subsystem == SDL_SYSWM_TYPE.SDL_SYSWM_X11) {
-            import std.conv : to;
-            return "x11:" ~ info.info.x11.window.to!string(16);
+        version(linux) {
+            SDL_SysWMinfo info;
+            auto res = SDL_GetWindowWMInfo(window, &info);
+            if (info.subsystem == SDL_SYSWM_TYPE.SDL_SYSWM_X11) {
+                import std.conv : to;
+                return "x11:" ~ info.info.x11.window.to!string(16);
+            }
         }
         return "";
     }
